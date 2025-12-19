c21b722551d455ed220258483365fe0f5071d7ea
Simar Preet Singh <simarpreetsinghkalra@gmail.com>
2025-12-19 18:33:09 +0530

    Update src/app/app.component.ts
    Co-authored-by: Christian Liebel <christian@liebel.org>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -78,7 +78,7 @@ export class AppComponent {
     const stream = session.promptStreaming(userPrompt);
     this.reply.set('');
     for await (const chunk of stream) {
-      this.reply.set(this.reply() + chunk);
+      this.reply.update(reply => reply + chunk);
     }
   }
 }
